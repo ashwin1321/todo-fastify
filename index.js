324b2b63e0aea6bb05b1a5b42910e4bbf8bb9ed1
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-30 10:44:59 +0545

    changes

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const fastify = require('fastify')({ logger: true });
-const { db } = require('./config/config');
-const todoRoutes = require('./routes/todoRoutes');
-const todoUser = require('./routes/todoUser');
+const fastify = require("fastify")({ logger: true });
+const { db } = require("./config/config");
+const todoRoutes = require("./routes/todoRoutes");
+const todoUser = require("./routes/todoUser");
 
 // register routes
-fastify.register(todoRoutes, { prefix: '/api' });
-fastify.register(todoUser, { prefix: '/auth' });
-
+fastify.register(todoRoutes, { prefix: "/api" });
+fastify.register(todoUser, { prefix: "/auth" });
 
 fastify.listen({ port: 3000 }, (err) => {
-    if (err) {
-        console.error('Error starting the server', err);
-        process.exit(1);
-    }
-    console.log('Server listening on port 3000');
+  if (err) {
+    console.error("Error starting the server", err);
+    process.exit(1);
+  }
+  console.log("Server listening on port 3000");
 });
