404a210b13e1acd86494aaeee895227e839a2fb4
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-30 10:36:44 +0545

    feat: auth using jwt

--- controller/userAuthController.js
+++ controller/userAuthController.js
@@ -1,77 +1,78 @@
-const pool = require('../config/config');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const pool = require("../config/config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // register a user
 const registerUser = async (request, reply) => {
+  const connection = await pool.promise().getConnection(); // Acquire a connection from the pool
+  try {
+    const { email, password } = request.body;
+    console.log(request.body);
 
-    const connection = await pool.promise().getConnection(); // Acquire a connection from the pool
-    try {
-        const { email, password } = request.body;
-        console.log(request.body)
+    const isUserExists = "SELECT * FROM todouser WHERE email = ?";
+    const [existingUser] = await connection.query(isUserExists, [email]); // Execute the query on the connection
 
-        const isUserExists = 'SELECT * FROM todouser WHERE email = ?';
-        const [existingUser] = await connection.query(isUserExists, [email]); // Execute the query on the connection
-
-        if (existingUser.length) {
-            return reply.code(409).send('User already exists');
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const query = 'INSERT INTO todouser (email, password) VALUES (?, ?)';
-        const [rows] = await connection.query(query, [email, hashedPassword]); // Execute the query on the connection
-
-        reply.code(201).send("user register successfully");
-
-    } catch (error) {
-        console.log('Error executing the query', error)
-        reply.code(500).send('Internal Server Error')
+    if (existingUser.length) {
+      return reply.code(409).send("User already exists");
     }
-    finally {
-        if (connection) {
-            connection.release();
-        }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = "INSERT INTO todouser (email, password) VALUES (?, ?)";
+    const [rows] = await connection.query(query, [email, hashedPassword]); // Execute the query on the connection
+
+    reply.code(201).send("user register successfully");
+  } catch (error) {
+    console.log("Error executing the query", error);
+    reply.code(500).send("Internal Server Error");
+  } finally {
+    if (connection) {
+      connection.release();
     }
-}
+  }
+};
 
 const loginUser = async (request, reply) => {
-    const connection = await pool.promise().getConnection(); // Acquire a connection from the pool
+  const connection = await pool.promise().getConnection(); // Acquire a connection from the pool
 
-    try {
-        const { email, password } = request.body;
+  try {
+    const { email, password } = request.body;
 
-        const query = 'SELECT * FROM todouser WHERE email = ?';
-        const [rows] = await connection.query(query, [email]); // Execute the query on the connection
+    const query = "SELECT * FROM todouser WHERE email = ?";
+    const [rows] = await connection.query(query, [email]); // Execute the query on the connection
 
-        if (!rows.length) {
-            return reply.code(401).send('User not found');
-        }
-
-        const user = rows[0];
-        const isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if (!isPasswordCorrect) {
-            return reply.code(401).send('Incorrect password');
-        }
-
-        // Generate access token
-        const accessToken = jwt.sign({ id: user.id }, "mySecretKey", { expiresIn: '5s' });
-
-        // Generate refresh token
-        const refreshToken = jwt.sign({ id: user.id }, "refreshSecretKey", { expiresIn: '1h' });
-
-        reply.code(200).send({ accessToken, refreshToken });
-    } catch (error) {
-        console.error('Error executing the query', error);
-        reply.code(500).send('Internal Server Error');
-    } finally {
-        if (connection) {
-            connection.release();
-        }
+    if (!rows.length) {
+      return reply.code(401).send("User not found");
     }
+
+    const user = rows[0];
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return reply.code(401).send("Incorrect password");
+    }
+
+    // Generate access token
+    const accessToken = jwt.sign({ id: user.id }, "mySecretKey", {
+      expiresIn: "5s",
+    });
+
+    // Generate refresh token
+    const refreshToken = jwt.sign({ id: user.id }, "refreshSecretKey", {
+      expiresIn: "1h",
+    });
+
+    reply.code(200).send({ accessToken, refreshToken });
+  } catch (error) {
+    console.error("Error executing the query", error);
+    reply.code(500).send("Internal Server Error");
+  } finally {
+    if (connection) {
+      connection.release();
+    }
+  }
 };
 
 module.exports = {
-    registerUser,
-    loginUser
+  registerUser,
+  loginUser,
 };
--- schemas/todoSchema.js
+++ schemas/todoSchema.js
@@ -1,96 +1,95 @@
-const todosController = require('../controller/todosController');
-
+const todosController = require("../controller/todosController");
 
 const todo = {
-    type: 'object',
-    properties: {
-        id: { type: 'integer' },
-        title: { type: 'string' },
-        completed: { type: 'boolean' }
-    }
-}
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    title: { type: "string" },
+    completed: { type: "boolean" },
+  },
+};
 
 // options for getting all todos
 const getTodosOpts = {
-    schema: {
-        response: {
-            200: {
-                type: 'array',
-                items: todo
-            }
-        }
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: todo,
+      },
     },
-    handler: todosController.getTodos
+  },
+  handler: todosController.getTodos,
 };
 
 // options for creating a new todo
 const createTodoOpts = {
-    schema: {
-        body: {
-            type: 'object',
-            required: ['title', 'completed'],
-            properties: {
-                title: { type: 'string' },
-                completed: { type: 'boolean' }
-            }
-        },
-        response: {
-            201: todo
-        }
+  schema: {
+    body: {
+      type: "object",
+      required: ["title", "completed"],
+      properties: {
+        title: { type: "string" },
+        completed: { type: "boolean" },
+      },
     },
-    handler: todosController.createTodo
+    response: {
+      201: todo,
+    },
+  },
+  handler: todosController.createTodo,
 };
 
 // options for updating a todo
 const updateTodoOpts = {
-    schema: {
-        params: {
-            type: 'object',
-            required: ['id'],
-            properties: {
-                id: { type: 'integer' }
-            }
-        },
-        body: {
-            type: 'object',
-            required: ['title', 'completed'],
-            properties: {
-                title: { type: 'string' },
-                completed: { type: 'boolean' }
-            }
-        },
-        response: {
-            200: todo
-        }
+  schema: {
+    params: {
+      type: "object",
+      required: ["id"],
+      properties: {
+        id: { type: "integer" },
+      },
     },
-    handler: todosController.updateTodo
+    body: {
+      type: "object",
+      required: ["title", "completed"],
+      properties: {
+        title: { type: "string" },
+        completed: { type: "boolean" },
+      },
+    },
+    response: {
+      200: todo,
+    },
+  },
+  handler: todosController.updateTodo,
 };
 
 // options for deleting a todo
 const deleteTodoOpts = {
-    schema: {
-        params: {
-            type: 'object',
-            required: ['id'],
-            properties: {
-                id: { type: 'integer' }
-            }
-        },
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    message: { type: 'string' }
-                }
-            }
-        }
+  schema: {
+    params: {
+      type: "object",
+      required: ["id"],
+      properties: {
+        id: { type: "integer" },
+      },
     },
-    handler: todosController.deleteTodo
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          message: { type: "string" },
+        },
+      },
+    },
+  },
+  handler: todosController.deleteTodo,
 };
 
 module.exports = {
-    getTodosOpts,
-    createTodoOpts,
-    updateTodoOpts,
-    deleteTodoOpts
+  getTodosOpts,
+  createTodoOpts,
+  updateTodoOpts,
+  deleteTodoOpts,
 };
