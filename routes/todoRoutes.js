bee09e4e9193a9c313b011c80c1188aab3e3c244
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-31 10:06:04 +0545

    spacing

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,13 +1,29 @@
-const schemas = require('../schemas/todoSchema');
-const validateUser = require('../middlewares/validateUser');
+const schemas = require("../schemas/todoSchema");
+const validateUser = require("../middlewares/validateUser");
 
 async function todoRoutes(fastify, options, done) {
-    fastify.get('/todos', { preValidation: [validateUser], schema: schemas.getTodosOpts.schema }, schemas.getTodosOpts.handler);
-    fastify.post('/todos', { preValidation: [validateUser], schema: schemas.createTodoOpts.schema }, schemas.createTodoOpts.handler);
-    fastify.put('/todos/:id', { preValidation: [validateUser], schema: schemas.updateTodoOpts.schema }, schemas.updateTodoOpts.handler);
-    fastify.delete('/todos/:id', { preValidation: [validateUser], schema: schemas.deleteTodoOpts.schema }, schemas.deleteTodoOpts.handler);
+  fastify.get(
+    "/todos",
+    { preValidation: [validateUser], schema: schemas.getTodosOpts.schema },
+    schemas.getTodosOpts.handler
+  );
+  fastify.post(
+    "/todos",
+    { preValidation: [validateUser], schema: schemas.createTodoOpts.schema },
+    schemas.createTodoOpts.handler
+  );
+  fastify.put(
+    "/todos/:id",
+    { preValidation: [validateUser], schema: schemas.updateTodoOpts.schema },
+    schemas.updateTodoOpts.handler
+  );
+  fastify.delete(
+    "/todos/:id",
+    { preValidation: [validateUser], schema: schemas.deleteTodoOpts.schema },
+    schemas.deleteTodoOpts.handler
+  );
 
-    done();
+  done();
 }
 
 module.exports = todoRoutes;
